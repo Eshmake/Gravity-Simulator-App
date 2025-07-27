52a0743b57f8dbf46913fd5007d28e0f0602e481
Eshmake <143656819+Eshmake@users.noreply.github.com>
2025-07-26 19:00:54 -0500

    updated

--- main2.py
+++ main2.py
@@ -6,6 +6,7 @@ from kivy.core.window import Window
 from kivy.core.image import Image as CoreImage
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import StringProperty
+from kivy.uix.spinner import Spinner
 import math
 
 WIDTH, HEIGHT = 800, 600
@@ -52,7 +53,50 @@ class MainMenuScreen(Screen):
 
         layout = BoxLayout(orientation='vertical', padding=20, spacing=20)
         
+        self.planet_options = list(PLANET_MASSES.keys())
+        self.selected_planet = self.planet_options[1]
 
+        self.spinner = Spinner(
+            text=self.selected_planet,
+            values=self.planet_options,
+            size_hint=(1, 0.2)
+        )
+
+        self.spinner.bind(text=self.select_planet)
+
+        start_btn = Button(text="Start Simulation", size_hint=(1, 0.2))
+        start_btn.bind(on_press=self.start_simulation)
+
+        quit_btn = Button(text="Quit", size_hint=(1, 0.2))
+        quit_btn.bind(on_press=App.get_running_app().stop)
+
+        layout.add_widget(Label(text="Select a Planet", size_hint=(1, 0.1)))
+        layout.add_widget(self.spinner)
+        layout.add_widget(start_btn)
+        layout.add_widget(quit_btn)
+
+        self.add_widget(layout)
+
+    def select_planet(self, spinner, text):
+        self.selected_planet = text
+
+    def start_simulation(self, instance):
+        sim_screen = self.manager.get_screen("sim")
+        sim_screen.set_planet(self.selected_planet)
+
+        self.manager.current = "sim"
+
+
+        
+class SimulationScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.sim_widget = GravitySimWidget()
+        self.add_widget(self.sim_widget)
+
+    def set_planet(self, name):
+        self.sim_widget.set_planet(name)
 
 
 Window.size = (WIDTH, HEIGHT)  # Optional for desktop preview
@@ -64,6 +108,12 @@ class Planet:
         self.radius = radius
         self.color = color
         self.name = planet_name
+        self.image = CoreImage(f"images/{self.name}.png").texture
+
+    def update_planet(self, newMass, newName):
+        self.mass = newMass
+        self.name = newName
+        self.image = CoreImage(f"images/{newName}.png").texture
 
 class Spaceship:
     def __init__(self, x, y, vx, vy, mass, radius, color=(1, 0, 0)):
@@ -82,8 +132,6 @@ class GravitySimWidget(Widget):
         self.drag_line = None
         self.drag_end = None
         self.bind(size=self.on_size)
-
-        self.planetTexture = CoreImage(f"images/{self.planet.name}.png").texture
 
         self.bg_texture = CoreImage("images/background.jpg").texture
 
@@ -152,6 +200,9 @@ class GravitySimWidget(Widget):
 
                 self.draw()
 
+    def set_planet(self, planet_name):
+        self.planet.update_planet(PLANET_MASSES[planet_name], planet_name)
+
     def draw(self):
         self.canvas.clear()
         with self.canvas:
@@ -162,7 +213,7 @@ class GravitySimWidget(Widget):
                       size=(self.width, self.height))
 
             # Draw planet
-            Rectangle(texture=self.planetTexture,
+            Rectangle(texture=self.planet.image,
                       pos=(self.planet.pos[0] - self.planet.radius,
                            self.planet.pos[1] - self.planet.radius),
                       size=(self.planet.radius * 2, self.planet.radius * 2))
@@ -176,7 +227,10 @@ class GravitySimWidget(Widget):
 
 class GravitySimApp(App):
     def build(self):
-        return GravitySimWidget()
+        sm = ScreenManager()
+        sm.add_widget(MainMenuScreen(name="menu"))
+        sm.add_widget(SimulationScreen(name="sim"))
+        return sm
 
 if __name__ == '__main__':
     GravitySimApp().run()
